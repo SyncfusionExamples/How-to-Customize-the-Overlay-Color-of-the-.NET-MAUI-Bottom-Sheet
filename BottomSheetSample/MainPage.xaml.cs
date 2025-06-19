db39c4bd8aa2e6d97f82c5608947323e767d762d
AravinthSundaramSF <aravinth.sundaram@syncfusion.com>
2025-06-19 15:39:45 +0530

    Customize the overlay color of bottomsheet.

---
+++ BottomSheetSample/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace BottomSheetSample
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_ItemTapped(object? sender, ItemTappedEventArgs e)
+        {
+            bottomSheet.BottomSheetContent.BindingContext = e.Item;
+            bottomSheet.Show();
+        }
+
+    }
+
+}
